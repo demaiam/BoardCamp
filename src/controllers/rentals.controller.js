5d99e1815d8c68203d83e41918074a9a2ffa1c59
andre <demaiam@gmail.com>
2023-07-30 19:13:05 -0300

    rentals fixes

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -41,16 +41,19 @@ export async function postRental(req, res) {
     if (!game.rows.length) return res.status(400).send("Game doesn't exist");
     
     const rentedGames = await db.query(`
-      SELECT COUNT(*) AS gameRented FROM rentals WHERE "gameId" = $1 AND "returnDate" = $2;`,
-      [gameId, null]
+      SELECT COUNT(*) AS "gameRented"
+        FROM rentals
+        WHERE "gameId" = $1
+        AND "returnDate" = $2;`,
+        [gameId, null]
     );
 
     if (game.rows[0].stockTotal - Number(rentedGames.rows[0].gameRented) <= 0) return res.status(400).send("Game not available");
 
     await db.query(`
       INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
-      VALUES ($1, $2, $3, $4, $5, $6, $7);`,
-      [customerId, gameId, dayjs().format('YYYY-MM-DD'), daysRented, null, daysRented * game.rows[0].pricePerDay, null]
+        VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+        [customerId, gameId, dayjs().format('YYYY-MM-DD'), daysRented, null, daysRented * game.rows[0].pricePerDay, null]
     );
 
     res.sendStatus(201);
